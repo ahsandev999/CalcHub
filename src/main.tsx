b21c93ab72d57affb9fd8ca33d3c9d9aca29c02a
Ahsan Mehmood <221057@students.au.edu.pk>
2026-07-19 12:41:57 +0500

    Fix unused imports in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import { ThemeProvider } from './context/ThemeContext';
